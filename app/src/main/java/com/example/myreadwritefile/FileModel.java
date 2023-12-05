d3ed5bdb3e7ab0ed5557f12ee520170806784582
LaelatulHodijah <hodijah2208@gmail.com>
2023-12-05 09:36:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myreadwritefile/FileModel.java
@@ -0,0 +1,20 @@
+package com.example.myreadwritefile;
+
+public class FileModel {
+    private String filename;
+    public String getFilename() {
+        return filename;
+    }
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    private String data;
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
